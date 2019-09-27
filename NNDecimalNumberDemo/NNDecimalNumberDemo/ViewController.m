dfc48df4315cf4f239b297eedb3f28133e9b43b0
顾海军 <guhaijun3@jd.com>
2019-09-27 12:45:39 +0800

    重定向 demo 输出到 textView

--- NNDecimalNumberDemo/NNDecimalNumberDemo/ViewController.m
+++ NNDecimalNumberDemo/NNDecimalNumberDemo/ViewController.m
@@ -11,7 +11,11 @@
 #import "DecimalNumberBehavior.h"
 #import "DecimalNumberGlobalBehavior.h"
 
+#define DLog(format, ...) printf("%s\n", [[NSString stringWithFormat:format, ##__VA_ARGS__] UTF8String]);
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *logTextView;
 
 @end
 
@@ -20,31 +24,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.logTextView.text = @"";
+    self.logTextView.editable = NO;
+    
+    [self redirectSTD:STDOUT_FILENO];
+    [self redirectSTD:STDERR_FILENO];
+    
+    [self decimalNumberDemo];
+ }
+
+- (void)decimalNumberDemo {
+    
     // 混合运算
     {
+        
         //( ( ( ( 3 - 2 + 5 ) × 3 ) × ( 10 × 2 ) ) ÷ 2 )
         NSString *v = @(3).nn_sub(@(2)).nn_add(@(5)).nn_mul(@(3)).nn_mul(@(10).nn_mul(@(2))).nn_div(@(2));
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     {
         //( ( ( ( 3 - 2 + 5 ) × 3 ) × ( 10 × 2 ) ) ÷ 2 )
         NSString *v = @"3".nn_sub(@"2").nn_add(@"5").nn_mul(@"3").nn_mul(@"10".nn_mul(@"2")).nn_div(@"2");
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     {
         //( ( ( ( 3 - 2 + 5 ) × 3 ) × ( 10 × 2 ) ) ÷ 2 )
         NSString *v = @(3).nn_sub(@"2").nn_add(@(5)).nn_mul(@"3").nn_mul(@(10).nn_mul(@"2")).nn_div(@(2));
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     {
         //( ( 2 × 5 + ( 2 × 2 ) ) ^ 2 )
         NSString *v = @"2".nn_mul(@(5)).nn_add(@"2".nn_mul(@"2")).nn_power(@(2));
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     {
         //( ( 2 × 5 + ( 2 × 2 ) ) × 10 ^ 2 )
         NSString *v = @(2).nn_mul(@"5").nn_add(@"2".nn_mul(@(2))).nn_mulPowerOf10(@(2));
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     // 值比较
@@ -52,16 +68,16 @@
         NSString *v0 = @"100";
         NSNumber *v1 = @(100.1);
         if ([v0 nn_decimalIsEqualTo:v1]) {
-            NSLog(@"v0 == v1 : v0 = %@, v1 = %@", v0, v1);
+            DLog(@"v0 == v1 : v0 = %@, v1 = %@", v0, v1);
         }
         else if ([v0 nn_decimalIsGreaterThan:v1]) {
-            NSLog(@"v0 > v1 : v0 = %@, v1 = %@", v0, v1);
+            DLog(@"v0 > v1 : v0 = %@, v1 = %@", v0, v1);
         }
         else if ([v0 nn_decimalIsGreaterThanOrEqualTo:v1]) {
-            NSLog(@"v0 <= v1 : v0 = %@, v1 = %@", v0, v1);
+            DLog(@"v0 <= v1 : v0 = %@, v1 = %@", v0, v1);
         }
         else if ([v0 nn_decimalIsLessThan:v1]) {
-            NSLog(@"v0 < v1 : v0 = %@, v1 = %@", v0, v1);
+            DLog(@"v0 < v1 : v0 = %@, v1 = %@", v0, v1);
         }
     }
     
@@ -70,12 +86,12 @@
         {
             [NSString nn_setDecimalNumberGlobalBehavior:[DecimalNumberGlobalBehavior class]];
             NSString *v = @"1".nn_div(@(3));
-            NSLog(@"%@ = %@", v, v.nn_formula);
+            DLog(@"%@ = %@", v, v.nn_formula);
         }
         {
             DecimalNumberBehavior *behavior = [DecimalNumberBehavior new];
             NSString *v = @"1".nn_behavior(behavior).nn_div(@(3)).nn_div(@(2));
-            NSLog(@"%@ = %@", v, v.nn_formula);
+            DLog(@"%@ = %@", v, v.nn_formula);
         }
         {
             NSDecimalNumberHandler *behavior = [[NSDecimalNumberHandler alloc] initWithRoundingMode:NSRoundPlain
@@ -85,11 +101,11 @@
                                                                                    raiseOnUnderflow:false
                                                                                 raiseOnDivideByZero:false];
             NSString *v = @"1".nn_behavior(behavior).nn_div(@(3));
-            NSLog(@"%@ = %@", v, v.nn_formula);
+            DLog(@"%@ = %@", v, v.nn_formula);
         }
         {
             NSString *v = @"1".nn_div(@(3));
-            NSLog(@"%@ = %@", v, v.nn_formula);
+            DLog(@"%@ = %@", v, v.nn_formula);
         }
     }
     
@@ -97,62 +113,87 @@
     {
         //( 2 × [UIView new] )
         NSString *v = @"2".nn_mul([UIView new]);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( 2 / 0 )
         NSString *v = @"2".nn_div(@"0");
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( nil / nil )
         NSString *v =  NN_Trust(nil).nn_div(nil);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( nil / 0 )
         NSString *v =  NN_Trust(nil).nn_div(@(0));
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( nil / [UIView new] )
         NSString *v =  NN_Trust(nil).nn_div([UIView new]);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( nil / [NSNull null] )
         NSString *v =  NN_Trust(nil).nn_div([NSNull null]);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( [NSNull null] / [NSNull null] )
         NSString *v =  [NSNull null].nn_div([NSNull null]);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( [NSNull null] / 0 )
         NSString *v =  [NSNull null].nn_div(@(0));
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( [NSNull null] / [UIView new] )
         NSString *v =  [NSNull null].nn_div([UIView new]);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
     
     {
         //( [NSNull null] / nil )
         NSString *v =  [NSNull null].nn_div(nil);
-        NSLog(@"%@ = %@", v, v.nn_formula);
+        DLog(@"%@ = %@", v, v.nn_formula);
     }
- }
+}
+
+- (void)redirectNotificationHandle:(NSNotification *)notification{
+    NSData *data = [[notification userInfo] objectForKey:NSFileHandleNotificationDataItem];
+    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+    self.logTextView.text = [NSString stringWithFormat:@"%@%@",self.logTextView.text, str];
+    NSRange range;
+    range.location = [self.logTextView.text length] - 1;
+    range.length = 0;
+    [self.logTextView scrollRangeToVisible:range];
+    
+    [[notification object] readInBackgroundAndNotify];
+}
+
+- (void)redirectSTD:(int )fd{
+    NSPipe * pipe = [NSPipe pipe] ;
+    NSFileHandle *pipeReadHandle = [pipe fileHandleForReading] ;
+    dup2([[pipe fileHandleForWriting] fileDescriptor], fd) ;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(redirectNotificationHandle:)
+                                                 name:NSFileHandleReadCompletionNotification
+                                               object:pipeReadHandle] ;
+    [pipeReadHandle readInBackgroundAndNotify];
+}
 
 @end

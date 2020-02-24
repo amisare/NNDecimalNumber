1b22809f6d2c6ebc83e666715fd7ec7fb7a87c6f
HaiJun Gu <243297288@qq.com>
2020-02-24 21:59:30 +0800

    modify: 修改目录结构，支持 carthage

--- Pods/Target Support Files/NNDecimalNumber/NNDecimalNumber-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NNDecimalNumber : NSObject
-@end
-@implementation PodsDummy_NNDecimalNumber
-@end
--- Pods/Target Support Files/Pods-NNDecimalNumberDemo/Pods-NNDecimalNumberDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NNDecimalNumberDemo : NSObject
-@end
-@implementation PodsDummy_Pods_NNDecimalNumberDemo
-@end
